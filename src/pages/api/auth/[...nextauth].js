143db531b27ea45a601121c9556003d0df1a6d22
muhamedbouda@gmail.com <muhamedbouda@gmail.com>
2023-05-06 12:52:53 +0300

    M

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,23 +8,19 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
-  ],
+    process.env.NEXTAUTH_SECRET],
 }
 
 export default NextAuth(authOptions)
 // import NextAuth from "next-auth"
-// import GithubProvider from "next-auth/providers/github"
+// import GoogleProvider from "next-auth/providers/google"
 
-// export const authOptions = {
-//   // Configure one or more authentication providers
+// export default NextAuth({
 //   providers: [
-//     GithubProvider({
-//       clientId: process.env.GITHUB_ID,
-//       clientSecret: process.env.GITHUB_SECRET,
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 //     }),
-//     // ...add more providers here
+//     secret: process.env.NEXTAUTH_SECRET
 //   ],
-// }
-
-// export default NextAuth(authOptions)
+// })
