9c877b249962faeeffacfd88551342706732a333
muhamedbouda@gmail.com <muhamedbouda@gmail.com>
2023-05-06 13:53:00 +0300

    Update AmazonFooter.js

--- src/pages/AmazonFooter.js
+++ src/pages/AmazonFooter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function AmazonFooter() {
   return (
-    <footer className=" bg-amazon_blue max-w-full">
+    <footer className=" bg-amazon_blue w-100">
       <div className="container mx-auto py-6 px-4">
         <div className="md:flex md:items-center md:justify-between">
           <div className="flex justify-center md:order-2">
@@ -13,24 +13,6 @@ function AmazonFooter() {
             </div>
           </div>
           <div className="mt-4  text-white md:mt-0 md:order-1 flex flex-row items-center justify-between space-x-40" >
-            <ul>
-                <li>Get to Know Us</li>
-                <li> About Amazon </li>
-                <li> Careers </li>
-                <li> Amazon Science </li>
-            </ul>
-            <ul>
-                <li>Get to Know Us</li>
-                <li> About Amazon </li>
-                <li> Careers </li>
-                <li> Amazon Science </li>
-            </ul>
-            <ul>
-                <li>Get to Know Us</li>
-                <li> About Amazon </li>
-                <li> Careers </li>
-                <li> Amazon Science </li>
-            </ul>
             <ul>
                 <li>Get to Know Us</li>
                 <li> About Amazon </li>
