260d0ea2e878b54199c3bada7bb266402a8501fc
muhamedbouda@gmail.com <muhamedbouda@gmail.com>
2023-05-06 12:19:19 +0300

    Update ProductFeed.js

--- src/pages/ProductFeed.js
+++ src/pages/ProductFeed.js
@@ -5,10 +5,6 @@ import { Carousel } from 'react-responsive-carousel';
 
 function ProductFeed({products,categories}) {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ded2ee0c (main)
     <>
             <div className="grid grid-flow-row-dense -mt-52 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto">
         <CatCard/>
@@ -18,13 +14,6 @@ function ProductFeed({products,categories}) {
           </div>
     <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto'>
         {products && products.slice(0,9).map(({id,title,description,category,image,price})=>
-<<<<<<< HEAD
-=======
-    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
-        {products && products.slice(0,4).map(({id,title,description,category,image,price})=>
->>>>>>> origin/main
-=======
->>>>>>> ded2ee0c (main)
         <Product
         key={id}
         id={id}
@@ -36,16 +25,7 @@ function ProductFeed({products,categories}) {
         />
         )}
         <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="" />
-<<<<<<< HEAD
-<<<<<<< HEAD
         {products && products.slice(6,products.length).map(({id,title,description,category,image,price})=>
-=======
-        <div className='md:col-span-2'>
-        {products && products.slice(4,5).map(({id,title,description,category,image,price})=>
->>>>>>> origin/main
-=======
-        {products && products.slice(6,products.length).map(({id,title,description,category,image,price})=>
->>>>>>> ded2ee0c (main)
         <Product
         key={id}
         id={id}
